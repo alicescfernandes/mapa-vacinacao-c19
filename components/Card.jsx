135edc5262553343aaa7e7ef07025f47b6d7cd62
Alice Fernandes <alicescfernandes@gmail.com>
2021-01-28 12:04:15 +0000

    more nextjs migration

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,5 @@
 import styles from './Card.module.scss';
 
-console.log({ styles });
-
 export function Card({ children }) {
 	return (
 		<>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
