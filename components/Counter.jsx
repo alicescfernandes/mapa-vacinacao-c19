4e5bd08902bbdf15c8dea1b40bdaef929910abb8
Alice Fernandes <alicescfernandes@gmail.com>
2021-01-29 19:06:08 +0000

    more data

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -3,12 +3,25 @@ import CountTo from 'react-count-to';
 import styles from './Card.module.scss';
 
 export function Counter({ to, title, subtitle }) {
+	let numberFormatter = new Intl.NumberFormat();
+
+	const fn = (value) => <span className={styles.counter}>{numberFormatter.format(value).replace(',', ' ')}</span>;
 	return (
 		<>
 			<Card>
 				<h2 className={styles.card_graph_title}>{title}</h2>
-				<p className={styles.card_graph_subtitle}>{title}</p>
-				<CountTo className={styles.counter} delay={10} to={to} speed={2000} />
+				<p className={styles.card_graph_subtitle}>{subtitle}</p>
+
+				{to === null ? (
+					<span className={styles.counter}>N/A</span>
+				) : (
+					<>
+						<CountTo delay={5} to={to} speed={3000}>
+							{fn}
+						</CountTo>
+						<p className={styles.card_graph_subtitle}>+123 que no dia anterior</p>
+					</>
+				)}
 			</Card>
 		</>
 	);
--- hooks/useData.js
+++ hooks/useData.js
@@ -1,13 +1,66 @@
 import { useEffect, useState } from 'react';
+import vaccines from '../data/vaccines.json';
 
 export function useData() {
+	let vals = [];
+	let labls = [];
 	let [labels, setLabels] = useState([]);
 	let [values, setValues] = useState([]);
 
+	let options = {
+		month: 'short',
+		day: 'numeric',
+	};
+	let f = new Intl.DateTimeFormat('pt-PT', options);
+
+	vaccines.forEach((el) => {
+		labls.push(f.format(new Date(el.Data)));
+		vals.push(el.Vacinados_Ac);
+	});
+
+	let statistics = {
+		getRaw: () => {
+			// console.log('vaccines', JSON.parse(JSON.stringify(vaccines)));
+			return vaccines;
+		},
+		getDesvioPadrao: () => {},
+
+		getMediaMovel: (dias) => {
+			let medias = [];
+			let labelsMedias = [];
+			for (let start = 0, end = dias; end < values.length; start += dias, end += dias) {
+				let sum = Math.round(values.slice(start, end).reduce((prev, current) => prev + current, 0) / dias);
+				medias.push(sum);
+				labelsMedias.push(labels[end]);
+				console.log(labels[start], labels[end]);
+			}
+			return {
+				values: medias,
+				labels: labelsMedias,
+			};
+		},
+
+		getVacinadosPorDia: () => {
+			let vacinadosPorDia = values.map((val, idx) => {
+				let nextDay = idx + 1;
+				if (idx > 0 && nextDay < values.length && values[nextDay] != null) {
+					console.log({ diaAaSeguir: [nextDay, values[nextDay]], diaAtua: [idx, val], diferenca: values[nextDay] - val });
+					return values[nextDay] - val;
+				} else {
+					return 0;
+				}
+			});
+			return {
+				values: vacinadosPorDia,
+				labels,
+			};
+		},
+	};
+
 	useEffect(() => {
-		setValues([1000, 10_000, 500, 1000, 1000, 1000, 8000]);
-		setLabels(['January', 'February', 'March', 'April', 'May', 'June', 'July']);
+		setValues(vals);
+		setLabels(labls);
 	}, []);
 
-	return { labels, values };
+	return { labels, values, statistics };
 }
