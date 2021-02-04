663ce73721a3eba7d18fb19705aa3fe201a92efa
Alice Fernandes <alicescfernandes@gmail.com>
2021-02-04 19:57:02 +0000

    more on automation

--- automation/make_image.js
+++ automation/make_image.js
@@ -1,15 +1,26 @@
 var fs = require('fs');
+var lastupdate = require('./ig/last-update.json');
 var contentHtml = fs.readFileSync('./image.html', 'utf8');
 
 const puppeteer = require('puppeteer');
 
 let d = new Date();
+d.setHours(0);
+d.setMinutes(0);
+d.setSeconds(0);
+d.setMilliseconds(0);
+
 (async () => {
-	const browser = await puppeteer.launch({ headless: true });
-	const page = await browser.newPage();
-	await page.setContent(contentHtml);
-	await page.setViewport({ width: 1080, height: 1080 });
-	await page.waitForTimeout(1000);
-	await page.screenshot({ path: `image-${d.getDay().toPrecision(1)}_${d.getMonth().toPrecision(1) + 1}_${d.getFullYear()}.png` });
-	await browser.close();
+	console.log(d.getTime() > lastupdate.date);
+	if (d.getTime() > lastupdate.date) {
+		d.lastupdate = d.getTime();
+		const browser = await puppeteer.launch({ headless: true });
+		const page = await browser.newPage();
+		await page.setContent(contentHtml);
+		await page.setViewport({ width: 1080, height: 1080 });
+		await page.waitForTimeout(1000);
+		await page.screenshot({ path: `ig/image-${d.getTime()}.png` });
+		await browser.close();
+		fs.writeFile('./ig/last-update.json', JSON.stringify(d), () => {});
+	}
 })();
