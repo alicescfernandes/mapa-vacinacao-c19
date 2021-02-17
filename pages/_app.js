2db7969bb0854d6d71e0406d551e12f3087fb9bf
Alice Fernandes <alicescfernandes@gmail.com>
2021-02-17 17:34:35 +0000

    adding more babel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../styles/globals.scss';
+import '@babel/polyfill';
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
