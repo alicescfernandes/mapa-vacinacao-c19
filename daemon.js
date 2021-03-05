c2a495f1d1d7e9bf90bf3346c16761ffc9f45f63
Alice Fernandes <alicescfernandes@gmail.com>
2021-03-05 21:55:23 +0000

    update daemon

--- daemon.js
+++ daemon.js
@@ -59,9 +59,7 @@ function publishEvent(type, data) {
 	});
 }
 
-function updateJSON() {
-	let updatedCases = false;
-	let updatedVaccines = false;
+async function updateJSON() {
 	//update the repo
 	shell.exec('git checkout develop');
 	shell.exec('git pull --rebase');
@@ -71,68 +69,65 @@ function updateJSON() {
 	date.setMilliseconds(0);
 	date.setSeconds(0);
 	date.setHours(0);
+
 	let dataLocalVacinas = JSON.parse(fs.readFileSync('./data/vaccines.json'));
 	let dataLocalVacinasV2 = JSON.parse(fs.readFileSync('./data/vaccines_v2.json'));
 	let dataLocalCases = JSON.parse(fs.readFileSync('./data/cases.json'));
 
-	let promises = [
-		fetch(
+	if (date.getTime() > dataLocalVacinas[dataLocalVacinas.length - 1].Data) {
+		let dataVacinas = await fetch(
 			'https://services5.arcgis.com/eoFbezv6KiXqcnKq/arcgis/rest/services/Covid19_Total_Vacinados/FeatureServer/0/query?f=json&where=FID=1&outFields=*&resultType=standard&cacheHint=true'
-		).then((res) => res.json()),
-		fetch(
+		).then((res) => res.json());
+
+		let sourceData = dataVacinas.features[0].attributes;
+		if (parseInt(sourceData.Vacinados_Ac) > dataLocalVacinas[dataLocalVacinas.length - 1].Vacinados_Ac) {
+			console.log('updating vaccines');
+			sourceData.Data = date.getTime();
+			dataLocalVacinas.push(sourceData);
+			fs.writeFileSync('./data/vaccines.json', JSON.stringify(dataLocalVacinas));
+			let item = dataLocalVacinas[dataLocalVacinas.length - 1];
+			item.Inoculacao1 = item.Inoculacao1_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Inoculacao1_Ac;
+			item.Inoculacao2 = item.Inoculacao2_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Inoculacao2_Ac;
+			item.Vacinados = item.Vacinados_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Vacinados_Ac;
+			dataLocalVacinasV2.push(item);
+			fs.writeFileSync('./data/vaccines_v2.json', JSON.stringify(dataLocalVacinasV2));
+			publishEvent('vacinas', dataVacinas.features[0].attributes);
+			updatedVaccines = true;
+
+			gitCommit('vaccines');
+			//Update twitter
+			shell.exec('yarn twitter');
+		} else {
+			console.log('not updating', 'vaccines', parseInt(sourceData.Vacinados_Ac), dataLocalVacinas[dataLocalVacinas.length - 1].Vacinados_Ac);
+		}
+	}
+
+	if (date.getTime() > dataLocalCases[dataLocalCases.length - 1].Data) {
+		let dataCasos = await fetch(
 			`https://services.arcgis.com/CCZiGSEQbAxxFVh3/arcgis/rest/services/COVID_Concelhos_DadosDiariosARS_VIEW2/FeatureServer/0/query?f=json&where=ARSNome='Nacional' and Data>'${
 				date.getMonth() + 1
 			}/${date.getDate()}/${date.getFullYear()}'&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=Data, ConfirmadosAcumulado, ConfirmadosNovos, Recuperados, Obitos,ObitosNovos,  RecuperadosNovos, VarRecuperados, Activos&orderByFields=data asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true`
-		).then((res) => res.json()),
-	];
-	Promise.all(promises)
-		.then(([dataVacinas, dataCasos]) => {
-			//Update vacines
-			let sourceData = dataVacinas.features[0].attributes;
-			if (parseInt(sourceData.Vacinados_Ac) > dataLocalVacinas[dataLocalVacinas.length - 1].Vacinados_Ac) {
+		).then((res) => res.json());
+		//Update cases
+		if (dataCasos.features.length > 0) {
+			console.log('updating cases');
+			sourceData = dataCasos.features[0].attributes;
+			if (parseInt(sourceData.OBJECTID) > dataLocalCases[dataLocalCases.length - 1].OBJECTID) {
+				console.log(new Date().toLocaleString(), 'updating');
 				sourceData.Data = date.getTime();
-				dataLocalVacinas.push(sourceData);
-				fs.writeFileSync('./data/vaccines.json', JSON.stringify(dataLocalVacinas));
-				let item = dataLocalVacinas[dataLocalVacinas.length - 1];
-				item.Inoculacao1 = item.Inoculacao1_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Inoculacao1_Ac;
-				item.Inoculacao2 = item.Inoculacao2_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Inoculacao2_Ac;
-				item.Vacinados = item.Vacinados_Ac - dataLocalVacinas[dataLocalVacinas.length - 2].Vacinados_Ac;
-				dataLocalVacinasV2.push(item);
-				fs.writeFileSync('./data/vaccines_v2.json', JSON.stringify(dataLocalVacinasV2));
-				publishEvent('vacinas', dataVacinas.features[0].attributes);
-				updatedVaccines = true;
+				dataLocalCases.push(sourceData);
+				fs.writeFileSync('./data/cases.json', JSON.stringify(dataLocalCases));
+				updatedCases = true;
+				publishEvent('casos', dataCasos.features[0].attributes);
 
-				gitCommit('vaccines');
-				//Update twitter
-				shell.exec('yarn twitter');
+				gitCommit('cases');
 			} else {
-				console.log('not updating', 'vaccines', parseInt(sourceData.Vacinados_Ac), dataLocalVacinas[dataLocalVacinas.length - 1].Vacinados_Ac);
+				console.log('not updating', 'cases', sourceData.OBJECTID, dataLocalCases[dataLocalCases.length - 1].OBJECTID);
 			}
-			//Update cases
-			if (dataCasos.features.length > 0) {
-				sourceData = dataCasos.features[0].attributes;
-				if (parseInt(sourceData.OBJECTID) > dataLocalCases[dataLocalCases.length - 1].OBJECTID) {
-					console.log(new Date().toLocaleString(), 'updating');
-					sourceData.Data = date.getTime();
-					dataLocalCases.push(sourceData);
-					fs.writeFileSync('./data/cases.json', JSON.stringify(dataLocalCases));
-					updatedCases = true;
-					publishEvent('casos', dataCasos.features[0].attributes);
-
-					gitCommit('cases');
-					//Update twitter
-				} else {
-					console.log('not updating', 'cases', sourceData.OBJECTID, dataLocalCases[dataLocalCases.length - 1].OBJECTID);
-				}
-			} else {
-				console.log('not updating', 'cases', dataCasos.features.length, dataLocalCases[dataLocalCases.length - 1].Data);
-			}
-			if (updatedCases || updatedVaccines) {
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+		} else {
+			console.log('not updating', 'cases', dataCasos.features.length, dataLocalCases[dataLocalCases.length - 1].Data);
+		}
+	}
 }
 
 console.log(new Date().toLocaleString(), 'daemon running');
