02b76f4b79c836e2ce06f5a5be539b36fb2e6861
Alice Fernandes <alicescfernandes@gmail.com>
2021-03-20 17:16:25 +0000

    More deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ git checkout master -f
 git pull
 yarn
 pm2 stop nextjs
-pm2 start yarn --name "nextjs" --interpreter bash -- start:80
-pm2 start yarn --name "nextjs" --interpreter bash -- start:443
+pm2 start yarn --name "nextjs" --interpreter bash -- start
 #pm2 restart nextjs
