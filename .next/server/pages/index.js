9564f1660742d5c0ec09a0b59937103c8bb687a6
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-03-25 12:52:53 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@d06159da00b166a4cbab43892b0e47a3fa8fc37e 🚀

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -8765,7 +8765,7 @@ function createObserver(options) {
 /***/ "vga7":
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"date\":1616673659755,\"dateSnsStartWeirdFormat\":\"15/03/2021\",\"dateSnsStart\":\"2021-03-15\",\"dateSns\":\"2020-03-21\",\"dateEcdc\":\"2021-03-21\",\"dateRt\":\"20210-03-13\"}");
+module.exports = JSON.parse("{\"date\":1616676656804,\"dateSnsStartWeirdFormat\":\"15/03/2021\",\"dateSnsStart\":\"2021-03-15\",\"dateSns\":\"2020-03-21\",\"dateEcdc\":\"2021-03-14\",\"dateRt\":\"20210-03-13\"}");
 
 /***/ }),
 
