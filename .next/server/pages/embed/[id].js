5ff600d0a786230a99aa0655797f05609447829a
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-03-28 11:01:42 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@28ee4e195087a09d60fd511968c1b8f2165f7092 🚀

--- .next/server/pages/embed/[id].js
+++ .next/server/pages/embed/[id].js
@@ -2214,7 +2214,7 @@ function DatePickerButton({
 /***/ "vga7":
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"date\":1616863205172,\"dateSnsStartWeirdFormat\":\"15/03/2021\",\"dateSnsStart\":\"2021-03-15\",\"dateSns\":\"2021-03-21T00:00:00+00:00\",\"dateEcdc\":\"2021-03-14\",\"dateRt\":\"20210-03-13\"}");
+module.exports = JSON.parse("{\"date\":1616929209817,\"dateSnsStartWeirdFormat\":\"15/03/2021\",\"dateSnsStart\":\"2021-03-15\",\"dateSns\":\"2021-03-21T00:00:00+00:00\",\"dateEcdc\":\"2021-03-14\",\"dateRt\":\"20210-03-13\"}");
 
 /***/ }),
 
