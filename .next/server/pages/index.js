728ffd9773906a3e8058fb219386fe50ddf41a86
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-04-09 19:29:06 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@c1133ed2481d8e7bdee35acc20a8888d6bf524da 🚀

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -2300,6 +2300,9 @@ function BarsVacinacaoArs({
           label: (tooltipItem, data) => {
             var label = data.datasets[tooltipItem.datasetIndex].label;
             return label + ': ' + Object(utils["c" /* formatNumber */])(parseInt(tooltipItem.value), false);
+          },
+          title: () => {
+            return '';
           }
         }
       },
@@ -3358,6 +3361,9 @@ function BarArs_CustomBarChart({
           label: (tooltipItem, data) => {
             var label = data.datasets[tooltipItem.datasetIndex].label;
             return label + ': ' + Object(utils["c" /* formatNumber */])(parseInt(tooltipItem.value), false);
+          },
+          title: () => {
+            return '';
           }
         }
       },
@@ -3585,22 +3591,6 @@ function BarArs({
               total: 5000
             })]
           })]
-        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])(external_react_bootstrap_["Row"], {
-          children: /*#__PURE__*/Object(jsx_runtime_["jsxs"])(external_react_bootstrap_["Col"], {
-            xs: 12,
-            lg: 6,
-            children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("div", {
-              className: 'subchart-data',
-              children: /*#__PURE__*/Object(jsx_runtime_["jsx"])("p", {
-                children: graphData['Madeira'].REGION
-              })
-            }), /*#__PURE__*/Object(jsx_runtime_["jsx"])(BarArs_CustomBarChart, {
-              colors: colors,
-              showHeading: true,
-              total: 400,
-              data: graphData['Madeira']
-            })]
-          })
         })]
       }) : ''
     })
@@ -5987,7 +5977,7 @@ const RegiaoContext = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.
 /***/ "vga7":
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"date\":1617996219287,\"dateSnsStartWeirdFormat\":\"29/03/2021\",\"dateSnsStart\":\"2021-03-29T00:00:00\",\"dateSns\":\"2021-04-04T00:00:00\",\"dateEcdc\":\"2021-04-04\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-04-4\",\"dateMadeiraCases\":\"2021-04-07\",\"dateAcores\":\"2021-04-08T00:00:00\",\"dateAcoresCases\":\"2021-04-08\"}");
+module.exports = JSON.parse("{\"date\":1617996453513,\"dateSnsStartWeirdFormat\":\"29/03/2021\",\"dateSnsStart\":\"2021-03-29T00:00:00\",\"dateSns\":\"2021-04-04T00:00:00\",\"dateEcdc\":\"2021-04-04\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-04-4\",\"dateMadeiraCases\":\"2021-04-07\",\"dateAcores\":\"2021-04-08T00:00:00\",\"dateAcoresCases\":\"2021-04-08\"}");
 
 /***/ }),
 
