450ad58607eabc4e7d0d67388541c883c65cd8a6
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-04-18 11:01:56 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@15271f59f5e86b5460c4572d6cfdd5d825506849 🚀

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -3766,7 +3766,7 @@ function createObserver(options) {
 /***/ "vga7":
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"date\":1618743609605,\"dateSnsStartWeirdFormat\":\"05/04/2021\",\"dateSnsStart\":\"2021-04-05T00:00:00\",\"dateSns\":\"2021-04-11T00:00:00\",\"dateEcdc\":\"2021-04-11T00:00:00\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-04-12\",\"dateMadeiraCases\":\"2021-04-12\",\"dateAcores\":\"2021-04-15T00:00:00\",\"dateAcoresCases\":\"2021-04-12\"}");
+module.exports = JSON.parse("{\"date\":1618743627674,\"dateSnsStartWeirdFormat\":\"05/04/2021\",\"dateSnsStart\":\"2021-04-05T00:00:00\",\"dateSns\":\"2021-04-11T00:00:00\",\"dateEcdc\":\"2021-04-11T00:00:00\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-04-12\",\"dateMadeiraCases\":\"2021-04-12\",\"dateAcores\":\"2021-04-15T00:00:00\",\"dateAcoresCases\":\"2021-04-12\"}");
 
 /***/ }),
 
