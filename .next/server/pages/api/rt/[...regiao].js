07b3f3a8f1388792fdcf70c87178ab7e59622c89
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-04-28 11:02:02 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@bccdd44dbb0341090aa124463e335914d3628cda 🚀

--- .next/server/pages/api/rt/[...regiao].js
+++ .next/server/pages/api/rt/[...regiao].js
@@ -49,7 +49,7 @@ module.exports =
 /******/
 /******/ 		// "0" is the signal for "already loaded"
 /******/ 		if(installedChunks[chunkId] !== 0) {
-/******/ 			var chunk = require("../../../" + ({}[chunkId]||chunkId) + "." + {"0":"9626677b38798c086bea","1":"05835cceeca6e2a7c472","2":"a2143ab38178f13c6b2c","3":"e29533244cfb4b5971b6","4":"6833373d5793f94dbd10","5":"8caa86656e767a204474","6":"f1c416ae8ad067756d69","7":"e700050f556ff7995620","8":"cb873f731df9a17a4785","9":"ddfd8e2b9783fb23d338"}[chunkId] + ".js");
+/******/ 			var chunk = require("../../../" + ({}[chunkId]||chunkId) + "." + {"0":"9626677b38798c086bea","1":"05835cceeca6e2a7c472","2":"a2143ab38178f13c6b2c","3":"e29533244cfb4b5971b6","4":"6833373d5793f94dbd10","5":"8caa86656e767a204474","6":"f1c416ae8ad067756d69","7":"e700050f556ff7995620","8":"cb873f731df9a17a4785","9":"7cf8c39c61d5eb4f1b32"}[chunkId] + ".js");
 /******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids;
 /******/ 			for(var moduleId in moreModules) {
 /******/ 				modules[moduleId] = moreModules[moduleId];
