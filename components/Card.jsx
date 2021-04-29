f30a487db405fc178d6de04c14375b0c671d7092
Alice Fernandes <alicescfernandes@gmail.com>
2021-04-29 19:34:41 +0100

    new counter cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,13 @@
 import styles from './Card.module.scss';
 import classNames from 'classnames';
 
-export function Card({ children, allowOverflow, isUpdating, textLeft }) {
+export function Card({ children, type, allowOverflow, isUpdating, textLeft, classes }) {
 	let styles2 = {};
 	styles2[styles.card_align_left] = textLeft;
 	styles2[styles.card_graph_updated] = isUpdating;
 	styles2[styles.card_chart] = allowOverflow;
-	let className = classNames(styles.card_graph, 'card-shadow', styles2);
+	styles2[styles.card_counter] = type === 'counter';
+	let className = classNames(styles.card_graph, styles2);
 	return (
 		<div className={styles.container}>
 			<div className={className}>{children}</div>
