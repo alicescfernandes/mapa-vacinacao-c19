b2e37582a3747d50cb77807724e249e6e5ac2b77
alicescfernandes <15971182+alicescfernandes@users.noreply.github.com>
2021-06-02 12:31:18 +0000

    Deploying to gh-pages from @ alicescfernandes/mapa-vacinacao-c19@eada4c9e507f3f587fa8091d3c1b54527ceb379f 🚀

--- .next/server/pages/acores.js
+++ .next/server/pages/acores.js
@@ -3604,7 +3604,7 @@ const RegiaoContext = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.
 /***/ "vga7":
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"date\":1622631628975,\"dateSnsStartWeirdFormat\":\"24/05/21\",\"dateSnsStart\":\"2021-05-24\",\"dateSns\":\"2021-05-28\",\"dateEcdc\":\"2021-05-23\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-05-31\",\"dateMadeiraCases\":\"2021-05-31\",\"dateAcores\":\"2021-05-28\",\"dateAcoresCases\":\"2021-06-01\",\"week\":21}");
+module.exports = JSON.parse("{\"date\":1622637003258,\"dateSnsStartWeirdFormat\":\"24/05/21\",\"dateSnsStart\":\"2021-05-24\",\"dateSns\":\"2021-05-28\",\"dateEcdc\":\"2021-05-23\",\"dateRt\":\"20210-03-28\",\"dateMadeira\":\"2021-05-31\",\"dateMadeiraCases\":\"2021-05-31\",\"dateAcores\":\"2021-05-28\",\"dateAcoresCases\":\"2021-06-01\",\"week\":21}");
 
 /***/ }),
 
