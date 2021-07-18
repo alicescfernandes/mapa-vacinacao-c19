3da9b1bdea4f76aeb9a05923e9412ecf0dc5bb8a
Alice Fernandes <alicescfernandes@gmail.com>
2021-07-18 14:35:36 +0100

    fix verce

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,15 @@
 // babel.config.js
 module.exports = function (api) {
-	const isServer = api.caller((caller) => caller.isServer);
-	const isCallerDevelopment = api.caller((caller) => caller.isDev);
+	//const isServer = api.caller((caller) => caller.isServer);
+	//const isCallerDevelopment = api.caller((caller) => caller.isDev);
 
 	const presets = [
 		[
 			'next/babel',
 			{
 				'preset-react': {
-					importSource: !isServer && isCallerDevelopment ? '@welldone-software/why-did-you-render' : 'react',
+					//importSource: !isServer && isCallerDevelopment ? '@welldone-software/why-did-you-render' : 'react',
+					importSource: 'react',
 				},
 				'preset-env': {
 					targets: {
