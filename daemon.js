83d3e057ca807bd63bea8df6b5ed389629010bea
Alice Fernandes <alicescfernandes@gmail.com>
2021-08-09 15:31:11 +0100

    fix json

--- daemon.js
+++ daemon.js
@@ -38,7 +38,6 @@ const pusher = new Pusher({
 });
 
 function gitCommit(name) {
-	return;
 	shell.exec('git add data/*');
 	if (shell.exec(`git commit -m  "covid update - ${name} - ${formatted}"`).code !== 0) {
 		shell.echo('Error: Git commit failed');
